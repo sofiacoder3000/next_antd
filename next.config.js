e8a8fbbd0345daf7acdc34dbb8c028149911e0ae
Jakeline Campos <chofi_14_jscc@hotmail.com>
2021-05-21 08:24:41 -0500

    First commit

---
+++ next.config.js
@@ -0,0 +1,24 @@
+// next.config.js
+const withAntdLess = require('next-plugin-antd-less');
+
+module.exports = withAntdLess({
+	// optional
+	modifyVars: { '@primary-color': '#04f' },
+	// optional
+	lessVarsFilePath: './styles/variables.less',
+	// optional
+	lessVarsFilePathAppendToEndOfContent: false,
+	// optional https://github.com/webpack-contrib/css-loader#object
+	cssLoaderOptions: {},
+
+	// Other Config Here...
+
+	webpack(config) {
+		return config;
+	},
+
+	future: {
+		// if you use webpack5
+		webpack5: true
+	}
+});
